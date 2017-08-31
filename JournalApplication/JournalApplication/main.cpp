2d3965a25a878bab516730ae53327be702e6140a
erickbordallo <ebortav@gmail.com>
2017-08-30 18:51:03 -0700

    Added comments at the begginig of the journal

--- JournalApplication/JournalApplication/main.cpp
+++ JournalApplication/JournalApplication/main.cpp
@@ -1,5 +1,11 @@
+//Name: Erick Tavares
+//Date: 25/08/2017
+//Description: Console app that allows you to create journals, add some entries and save as a text file.
+
+
 #include <iostream>
 #include "JournalApp.h"
+
 
 using namespace std;
 
